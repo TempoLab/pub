5cdb6488ee70fbfdce1d70ca65dcbc0b60e570eb
Matthew Solomon <matt@Tempo>
2023-01-10 19:04:43 +1300

    ls fix ???

--- islands/ToggleLightDark.tsx
+++ islands/ToggleLightDark.tsx
@@ -1,36 +1,12 @@
 import { useState } from "preact/hooks";
-import { DarkButton } from "../components/DarkButton.tsx";
-import { IS_BROWSER } from "$fresh/runtime.ts";
-import { setValueInLocalStorage, getValueInLocalStorage, } from "../utils/safeLocalStorageGetSet.ts"
-import {
-    GrActions,
-    GrMoon,
-} from "https://deno.land/x/react_icons@0.2.3/gr/mod.ts";
 
-function getMode(theme): "light" | "dark" {
-    if (!IS_BROWSER) {
-        return "light";
-    }
-    // if (getValueInLocalStorage(theme) === "dark") {
-    //     return "dark";
-    // }
-    // if (getValueInLocalStorage(theme)) {
-    //     return "light";
-    // }
-    return "light";
-}
-
-export default () => {
-    console.log(useState('light'))
-    //const [mode, setMode] = useState('light')//useState(getMode());
-    return (
-        <div class="flex gap-2 w-full">
-            <DarkButton onClick={() => {
-
-            }}>
-                {getValueInLocalStorage("theme") === "dark" && <GrActions />}
-                {getValueInLocalStorage("theme") === "light" && <GrMoon />}
-            </DarkButton>
-        </div>
-    );
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  );
 }
