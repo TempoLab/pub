c20dea95310c7069815ca13e3234bfe1308b8ea5
Matthew Solomon <matt@Tempo>
2023-01-10 18:51:29 +1300

    ls fix ???

--- islands/ToggleLightDark.tsx
+++ islands/ToggleLightDark.tsx
@@ -1,12 +1,14 @@
 import { DarkButton } from "../components/DarkButton.tsx";
+import { setValueInLocalStorage, getValueInLocalStorage, } from "../utils/safeLocalStorageGetSet.ts"
+
 export default () => {
     return (
         <div class="flex gap-2 w-full">
             <DarkButton onClick={() => {
 
             }}>
-                {/* {getValueInLocalStorage("theme") === "dark" && <GrActions />}
-                {getValueInLocalStorage("theme") === "light" && <GrMoon />} */}
+                {getValueInLocalStorage("theme") === "dark" && <GrActions />}
+                {getValueInLocalStorage("theme") === "light" && <GrMoon />}
             </DarkButton>
         </div>
     );
