0d4acf4d631c89d524c288bc138843e0efcf8e81
Matthew Solomon <matt@Tempo>
2023-01-10 18:53:44 +1300

    ls fix ???

--- islands/ToggleLightDark.tsx
+++ islands/ToggleLightDark.tsx
@@ -1,9 +1,23 @@
 import { DarkButton } from "../components/DarkButton.tsx";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 import { setValueInLocalStorage, getValueInLocalStorage, } from "../utils/safeLocalStorageGetSet.ts"
 import {
     GrActions,
     GrMoon,
 } from "https://deno.land/x/react_icons@0.2.3/gr/mod.ts";
+
+function getMode(theme): "light" | "dark" {
+    if (!IS_BROWSER) {
+        return "light";
+    }
+    // if (getValueInLocalStorage(theme) === "dark") {
+    //     return "dark";
+    // }
+    // if (getValueInLocalStorage(theme)) {
+    //     return "light";
+    // }
+    return "light";
+}
 
 export default () => {
     return (
